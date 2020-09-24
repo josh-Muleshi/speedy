aaf7e3ee4f3943bf100e3c938481517d796b6b54
josh muleshi <jmuleshi2@gmail.com>
2020-09-24 13:43:55 +0100

    first commit

---
+++ app/src/main/java/com/example/speedy/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.speedy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.luseen.spacenavigation.SpaceItem;
+import com.luseen.spacenavigation.SpaceNavigationView;
+import com.luseen.spacenavigation.SpaceOnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    SpaceNavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        navigationView = findViewById(R.id.space);
+
+        navigationView.initWithSaveInstanceState(savedInstanceState);
+        navigationView.addSpaceItem(new SpaceItem("Home", R.drawable.ic_home_black_24));
+        navigationView.addSpaceItem(new SpaceItem("Settings", R.drawable.ic_settings_black_24));
+
+        navigationView.setSpaceOnClickListener(new SpaceOnClickListener() {
+            @Override
+            public void onCentreButtonClick() {
+                Toast.makeText(MainActivity.this,"onCentreButtonClick", Toast.LENGTH_SHORT).show();
+                navigationView.setCentreButtonSelectable(true);
+            }
+
+            @Override
+            public void onItemClick(int itemIndex, String itemName) {
+                Toast.makeText(MainActivity.this, itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemReselected(int itemIndex, String itemName) {
+                Toast.makeText(MainActivity.this, itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
